52bef6a1c742fba7966b8d938f18e8d1f4ecad49
kayoMichael <michaelxkayo@hotmail.com>
2024-05-22 08:51:11 -0400

    feat: highlighted projects

--- src/components/hero/highlightedProjects.tsx
+++ src/components/hero/highlightedProjects.tsx
@@ -2,8 +2,11 @@
 import React, { useRef } from 'react';
 
 import { useInView, motion } from 'framer-motion';
+import { ChevronRight } from 'lucide-react';
+import Link from 'next/link';
 
 import ProjectCard from '../projects/ProjectsCard';
+import { Button } from '../ui/button';
 
 import { Projects } from '@/src/utils/projects';
 
@@ -44,22 +47,34 @@ const HighlightedProjects = () => {
             What I&apos;ve been working on
           </p>
         </motion.div>
+        <div className='my-4 flex items-center justify-start'>
+          <Link href='/projects'>
+            <Button variant='outline'>
+              See All Projects {<ChevronRight className='h-4 w-4' />}
+            </Button>
+          </Link>
+        </div>
+        <motion.div
+          animate={{ y: 0, opacity: 1 }}
+          className='my-4 md:flex items-center md:my-8 md:grid-cols-2 gap-8'
+          initial={{ y: 40, opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          {Projects.map((project) => (
+            <div key={project.github}>
+              <ProjectCard
+                date={project.date}
+                description={project.description}
+                githubLink={project.github}
+                image={project.image}
+                name={project.title}
+                techStack={project.techStack}
+                url={project.url}
+              />
+            </div>
+          ))}
+        </motion.div>
       </motion.div>
-      <div className='grid lg:grid-cols-2 lg:w-3/4 w-full md:justify-start'>
-        {Projects.map((project) => (
-          <div key={project.github}>
-            <ProjectCard
-              date={project.date}
-              description={project.description}
-              githubLink={project.github}
-              image={project.image}
-              name={project.title}
-              techStack={project.techStack}
-              url={project.url}
-            />
-          </div>
-        ))}
-      </div>
     </div>
   );
 };
